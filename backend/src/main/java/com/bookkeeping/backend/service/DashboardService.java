22531d7d9f2e2b89fae429104105b841e6ff266c
mimi0007 <afsanaaziz.mimi@gmail.com>
2022-09-10 19:23:28 +0600

    "Add getReconciliationYearGross Method"

--- backend/src/main/java/com/bookkeeping/backend/service/DashboardService.java
+++ backend/src/main/java/com/bookkeeping/backend/service/DashboardService.java
@@ -1,8 +1,11 @@
 package com.bookkeeping.backend.service;
 
+import com.bookkeeping.backend.entity.ReconciliationYearGross;
 import com.bookkeeping.backend.entity.YearGross;
 
 
 public interface DashboardService {
-    public YearGross getYearGross(Integer year);
+    YearGross getYearGross(Integer year);
+
+    ReconciliationYearGross getReconciliationYearGross(Integer year);
 }
