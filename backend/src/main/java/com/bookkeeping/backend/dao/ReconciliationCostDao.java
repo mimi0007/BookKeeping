c9c975705e35a7da36a02153715858b69d554765
mimi0007 <afsanaaziz.mimi@gmail.com>
2022-09-10 19:21:52 +0600

    "Add ReconciliationCostDao"

---
+++ backend/src/main/java/com/bookkeeping/backend/dao/ReconciliationCostDao.java
@@ -0,0 +1,12 @@
+package com.bookkeeping.backend.dao;
+
+import com.bookkeeping.backend.entity.ReconciliationCost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReconciliationCostDao extends JpaRepository<ReconciliationCost, Integer> {
+    List<ReconciliationCost> findAllCostByYear(Integer year);
+}
