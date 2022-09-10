7a9ad8da02868e0b8e26cdb150c288ac8d7c9f48
mimi0007 <afsanaaziz.mimi@gmail.com>
2022-09-10 19:18:46 +0600

    "Refactor YearGross Entity with Proper Annotation"

--- backend/src/main/java/com/bookkeeping/backend/entity/YearGross.java
+++ backend/src/main/java/com/bookkeeping/backend/entity/YearGross.java
@@ -1,62 +1,18 @@
 package com.bookkeeping.backend.entity;
 
+import lombok.*;
+
 import java.util.Map;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class YearGross {
-    public Income income;
-    public Cost cost;
-    public Map<String, Integer> cumulativeIncome;
-    public Map<String, Integer> cumulativeCost;
-    public Map<String, Integer> result;
-
-    public YearGross(Income income, Cost cost, Map<String, Integer> cumulativeIncome, Map<String,
-            Integer> cumulativeCost, Map<String, Integer> result) {
-        this.income = income;
-        this.cost = cost;
-        this.cumulativeIncome = cumulativeIncome;
-        this.cumulativeCost = cumulativeCost;
-        this.result = result;
-    }
-
-    public Income getIncome() {
-        return income;
-    }
-
-    public void setIncome(Income income) {
-        this.income = income;
-    }
-
-    public Cost getCost() {
-        return cost;
-    }
-
-    public void setCost(Cost cost) {
-        this.cost = cost;
-    }
-
-    public Map<String, Integer> getCumulativeIncome() {
-        return cumulativeIncome;
-    }
-
-    public void setCumulativeIncome(Map<String, Integer> cumulativeIncome) {
-        this.cumulativeIncome = cumulativeIncome;
-    }
-
-    public Map<String, Integer> getCumulativeCost() {
-        return cumulativeCost;
-    }
-
-    public void setCumulativeCost(Map<String, Integer> cumulativeCost) {
-        this.cumulativeCost = cumulativeCost;
-    }
-
-    public Map<String, Integer> getResult() {
-        return result;
-    }
-
-    public void setResult(Map<String, Integer> result) {
-        this.result = result;
-    }
-
-
+    private Income income;
+    private Cost cost;
+    private Map<String, Integer> cumulativeIncome;
+    private Map<String, Integer> cumulativeCost;
+    private Map<String, Integer> result;
 }
