c315f2827482f089980041b34ffe2b89ec6551cb
mimi0007 <afsanaaziz.mimi@gmail.com>
2022-09-11 22:25:06 +0600

    "Refactor Income Entity"

--- backend/src/main/java/com/bookkeeping/backend/entity/Income.java
+++ backend/src/main/java/com/bookkeeping/backend/entity/Income.java
@@ -2,61 +2,23 @@ package com.bookkeeping.backend.entity;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
 @Table(name = "income")
 public class Income {
-    @Id
-    @Column(nullable = false)
-    private Integer year;
-    private Integer jan;
-    private Integer feb;
-    private Integer mar;
-    private Integer apr;
-    private Integer may;
-    private Integer jun;
-    private Integer jul;
-    private Integer aug;
-    private Integer sep;
-    private Integer oct;
-    private Integer nov;
-    private Integer dec;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-    public Integer getMonthValue(String month) {
-        switch (month) {
-            case "jan":
-                return jan;
-            case "feb":
-                return feb;
-            case "mar":
-                return mar;
-            case "apr":
-                return apr;
-            case "may":
-                return may;
-            case "jun":
-                return jun;
-            case "jul":
-                return jul;
-            case "aug":
-                return aug;
-            case "sep":
-                return sep;
-            case "oct":
-                return oct;
-            case "nov":
-                return nov;
-            default:
-                return dec;
-        }
-    }
+    @Id
+    @Column(name = "year", nullable = false)
+    private Integer year;
+
+    @OneToOne
+    @JoinColumn(name = "months_id", referencedColumnName = "id")
+    private Months months;
 }
