98e00a843a70a2737ad57103146f1c490be33bc0
mimi0007 <afsanaaziz.mimi@gmail.com>
2022-09-11 22:25:31 +0600

    "Add Months Entity"

---
+++ backend/src/main/java/com/bookkeeping/backend/entity/Months.java
@@ -0,0 +1,62 @@
+package com.bookkeeping.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "months")
+public class Months {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private Integer jan;
+    private Integer feb;
+    private Integer mar;
+    private Integer apr;
+    private Integer may;
+    private Integer jun;
+    private Integer jul;
+    private Integer aug;
+    private Integer sep;
+    private Integer oct;
+    private Integer nov;
+    private Integer dec;
+
+    public Integer getMonthValue(String month) {
+        switch (month) {
+            case "jan":
+                return jan;
+            case "feb":
+                return feb;
+            case "mar":
+                return mar;
+            case "apr":
+                return apr;
+            case "may":
+                return may;
+            case "jun":
+                return jun;
+            case "jul":
+                return jul;
+            case "aug":
+                return aug;
+            case "sep":
+                return sep;
+            case "oct":
+                return oct;
+            case "nov":
+                return nov;
+            default:
+                return dec;
+        }
+    }
+}
