5d2f3c3f5708f64a3b01124b8078ec5e3fcf7f93
mimi0007 <afsanaaziz.mimi@gmail.com>
2022-09-12 22:06:47 +0600

    "Update ReconciliationCostDao"

--- backend/src/main/java/com/bookkeeping/backend/dao/ReconciliationCostDao.java
+++ backend/src/main/java/com/bookkeeping/backend/dao/ReconciliationCostDao.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface ReconciliationCostDao extends JpaRepository<ReconciliationCost, Integer> {
     List<ReconciliationCost> findAllCostByYear(Integer year);
+    ReconciliationCost findReconciliationIncomeByYearAndType(Integer year, String type);
 }
