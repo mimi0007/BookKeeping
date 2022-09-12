7baeb6987ed92966bd9cc76a4921122f870b7e58
mimi0007 <afsanaaziz.mimi@gmail.com>
2022-09-12 23:10:53 +0600

    "Add getYears Method in DashboardController"

--- backend/src/main/java/com/bookkeeping/backend/controller/DashboardController.java
+++ backend/src/main/java/com/bookkeeping/backend/controller/DashboardController.java
@@ -6,9 +6,13 @@ import com.bookkeeping.backend.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/dashboard")
 public class DashboardController {
     @Autowired
     private DashboardService dashboardService;
@@ -21,5 +25,10 @@ public class DashboardController {
     @GetMapping("/reconciliation-income-cost/{year}")
     public ReconciliationYearGross getReconGrossIncomeAndCost(@PathVariable Integer year) {
         return this.dashboardService.getReconciliationYearGross(year);
+    }
+
+    @GetMapping("/years")
+    public List<Integer> getYears() {
+        return this.dashboardService.getYears();
     }
 }
