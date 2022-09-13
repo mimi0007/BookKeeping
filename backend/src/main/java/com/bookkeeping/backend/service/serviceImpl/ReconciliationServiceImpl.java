03c5a8fbf99fcae661b3a5972b55ce94bcd98f3a
mimi0007 <afsanaaziz.mimi@gmail.com>
2022-09-13 12:45:18 +0600

    "Refactor ReconciliationServiceImpl"

--- backend/src/main/java/com/bookkeeping/backend/service/serviceImpl/ReconciliationServiceImpl.java
+++ backend/src/main/java/com/bookkeeping/backend/service/serviceImpl/ReconciliationServiceImpl.java
@@ -1,22 +1,29 @@
 package com.bookkeeping.backend.service.serviceImpl;
 
 import com.bookkeeping.backend.dao.MonthsDao;
-import com.bookkeeping.backend.dao.ReconciliationCostDao;
+import com.bookkeeping.backend.dao.ReconciliationExpenseDao;
 import com.bookkeeping.backend.dao.ReconciliationIncomeDao;
 import com.bookkeeping.backend.entity.MonthInfoAccess;
-import com.bookkeeping.backend.entity.ReconciliationCost;
+import com.bookkeeping.backend.entity.ReconciliationExpense;
 import com.bookkeeping.backend.entity.ReconciliationIncome;
 import com.bookkeeping.backend.service.ReconciliationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static com.bookkeeping.backend.utility.Constants.MONTH_NAMES;
+
 @Service
 public class ReconciliationServiceImpl implements ReconciliationService {
+    private final List<String> allMonths = Arrays.asList(MONTH_NAMES);
     @Autowired
     ReconciliationIncomeDao reconciliationIncomeDao;
 
     @Autowired
-    ReconciliationCostDao reconciliationCostDao;
+    ReconciliationExpenseDao reconciliationExpenseDao;
 
     @Autowired
     MonthsDao monthsDao;
@@ -28,9 +35,9 @@ public class ReconciliationServiceImpl implements ReconciliationService {
     }
 
     @Override
-    public void addReconciliationCost(ReconciliationCost reconciliationCost) {
-        monthsDao.save(reconciliationCost.getMonths());
-        reconciliationCostDao.save(reconciliationCost);
+    public void addReconciliationExpense(ReconciliationExpense reconciliationExpense) {
+        monthsDao.save(reconciliationExpense.getMonths());
+        reconciliationExpenseDao.save(reconciliationExpense);
     }
 
     @Override
@@ -40,6 +47,10 @@ public class ReconciliationServiceImpl implements ReconciliationService {
         String monthName = monthInfoAccess.getMonthName();
         String type = monthInfoAccess.getType();
 
+        if (!allMonths.contains(monthName)) {
+            throw new NoSuchElementException("Month Does not Exist");
+        }
+
         ReconciliationIncome reconciliationIncome =
                 reconciliationIncomeDao.findReconciliationIncomeByYearAndType(year, type);
 
@@ -48,14 +59,18 @@ public class ReconciliationServiceImpl implements ReconciliationService {
     }
 
     @Override
-    public void updateReconciliationCost(MonthInfoAccess monthInfoAccess) {
+    public void updateReconciliationExpense(MonthInfoAccess monthInfoAccess) {
         Integer year = monthInfoAccess.getYear();
         Integer monthValue = monthInfoAccess.getMonthValue();
         String monthName = monthInfoAccess.getMonthName();
         String type = monthInfoAccess.getType();
 
-        ReconciliationCost reconciliationCost =
-                reconciliationCostDao.findReconciliationIncomeByYearAndType(year, type);
+        if (!allMonths.contains(monthName)) {
+            throw new NoSuchElementException("Month Does not Exist");
+        }
+
+        ReconciliationExpense reconciliationCost =
+                reconciliationExpenseDao.findReconciliationExpenseByYearAndType(year, type);
 
         reconciliationCost.getMonths().setMonthValue(monthName, monthValue);
         monthsDao.save(reconciliationCost.getMonths());
